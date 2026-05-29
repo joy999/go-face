ebfbb48caae6993b29aa7e6fcc56c9c7e3630417
Zhu Hua <54zhua@gmail.com>
2026-05-29 10:41:41 +0800

    feat: add vendor-aware lib path and runtime rpath for Linux builds
    - Add `../../../../lib/inspireface/lib/...` to LDFLAGS so vendored
      packages can locate shared libs placed under project root.
    - Add `$ORIGIN` and `$ORIGIN/lib` rpath for easier deployment.
    - Apply consistently to linux_x86, linux_arm64, and linux_rk3588.

--- cgo_linux_arm64.go
+++ cgo_linux_arm64.go
@@ -4,9 +4,11 @@ package goface
 
 /*
 #cgo CFLAGS: -I${SRCDIR}/third_party/inspireface/include
-#cgo LDFLAGS: -L${SRCDIR}/third_party/inspireface/lib/linux_aarch64_rk3588 -L/usr/local/lib -L/usr/lib -L/usr/lib/aarch64-linux-gnu -lInspireFace
-#cgo LDFLAGS: -L${SRCDIR}/third_party/inspireface/lib/linux_aarch64_rk3588 -L/usr/local/lib -L/usr/lib -L/usr/lib/aarch64-linux-gnu -lrknnrt
+#cgo LDFLAGS: -L${SRCDIR}/../../../../lib/inspireface/lib/linux_aarch64_rk3588 -L${SRCDIR}/third_party/inspireface/lib/linux_aarch64_rk3588 -L/usr/local/lib -L/usr/lib -L/usr/lib/aarch64-linux-gnu -lInspireFace
+#cgo LDFLAGS: -L${SRCDIR}/../../../../lib/inspireface/lib/linux_aarch64_rk3588 -L${SRCDIR}/third_party/inspireface/lib/linux_aarch64_rk3588 -L/usr/local/lib -L/usr/lib -L/usr/lib/aarch64-linux-gnu -lrknnrt
 #cgo LDFLAGS: -lm -ldl
+#cgo LDFLAGS: -Wl,-rpath,\$ORIGIN
+#cgo LDFLAGS: -Wl,-rpath,\$ORIGIN/lib
 #cgo LDFLAGS: -Wl,-rpath,${SRCDIR}/third_party/inspireface/lib/linux_aarch64_rk3588
 #cgo LDFLAGS: -Wl,-rpath,/usr/lib/aarch64-linux-gnu
 */
